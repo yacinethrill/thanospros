1988f6627ad269ed71cac09e106c45e21c6aa8a2
yacinethrill <121810167+yacinethrill@users.noreply.github.com>
2023-01-09 20:29:05 +0100

    Update config.py

--- config.py
+++ config.py
@@ -8,17 +8,17 @@ class Development(Config):
     # the name to display in your alive message
     ALIVE_NAME = "rishabh"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "postgres://jrycfssc:fEZMYWiOcO9vB8cIq1_Z97GhW3TiTppJ@tiny.db.elephantsql.com/jrycfssc"
+    DB_URI = "postgres://fncceyfs:V5dhNhp4Ok0LE5dYugLZRJoh64ui31NQ@mel.db.elephantsql.com/fncceyfs"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "your session"
+    STRING_SESSION = " 1BJWap1sBu2VoiB-jsGWkVWgepbPt7ailybsxjNe5d035jq_hjd9_StkebUbjrHsvPAgTx7kuOm3rIqUSjf-pZu01_Buw-9r43SUkwJ0RHjZFbgdJjzRenze_x5nRTL6QEA8B85FwkwUuLunyTWAazSxS93JI6pEQ4t9tlzdCN3BJ8u0rCFS1akj3U_5rHl5K89uhr9d13utZ1qWzWuQVxe4MsWVG5lBpwJm8Fx7GbTQgCCdWalWHz89NyUhvSTUTYPZZYIecQAtJDwoMf9jsT_uuffyoMsNuwyXBI41JBIlgEJ6XbiYOTlX2g_GHVrYSmmPioJUrRdVnRyZNH1zI_z0fwz8qrNI="
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "your token"
+    TG_BOT_TOKEN = "5812743746:AAH5MB7SpMSyrTdHVax5CNIRHjUNNFgm1eI"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID =
+    PRIVATE_GROUP_BOT_API_ID = -1001893273254
     # command handler
     COMMAND_HAND_LER = "."
     VCMODE = "True"
-    VC_SESSION = "your 2nd id telethon session" #note don't use maim account to vc player
+    VC_SESSION = "1BJWap1sBu8ZTRWOGFAO-UNhcv6wjJ2x9OAQrKRYSX_NgeZ9xDPNahGz68NEfH0_zNaFu0_B9Krybg-Yb9eD998Q5GLc6HHh29h61aG7mKG8Q8eigXWl5MEk-wbqPSkXQogodps6PZtydbnato4w2bOos0zNSVPqMhVrfTUYtPw_eUxog0Le-wp5sO0boGumklPpijSNAkrrHKXgryxKg33RrgxCeZyGFKaVc-_MUagwysJgBLStKoMuru1QyXpwQa-JLzCy2I2Tmo6kOtYRETQBCkvrmf3FL3VyoBD7kdiNxm0Ul4FQGBlWfVqzzgx7v0B5GgrTD_0ABGEQgrUD-Xa4-AppXi6o=" #note don't use maim account to vc player
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
     # External plugins repo
